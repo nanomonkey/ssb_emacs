9d6d9ef16d1703541d1ac07115d6700c1ce77bec
Scott Garrison <scott@allpowerlabs.org>
2017-05-07 21:41:10 -0700

    improvements to message display

--- ssb_emacs.el
+++ ssb_emacs.el
@@ -1,6 +1,6 @@
 (require 'json)
 
-(defun ssb-start-server () (shell-command-to-string "sbot server"))
+(defun ssb-start-server () (async-shell-command "sbot server"))
 
 (defun ssb-whoami ()
   (cdr (assoc 'id (json-read-from-string (shell-command-to-string "sbot whoami")))))
@@ -25,24 +25,42 @@
   (json-read-from-string (ssb-get (cdr (assoc 'previous message_id)))))
 
 (defun ssb-message-type (message_data) 
-  (cdr (assoc 'type (cdr (assoc 'content message_data)))))
+  (cdr 
+   (assoc 'type 
+          (cdr 
+           (assoc 'content 
+                  (cdr (assoc 'value 
+                              (json-read-from-string message_data))))))))
 
+(defun ssb-message-timestamp (message_data)
+  (format-time-string "%D %H:%M"
+   (cdr (assoc 'timestamp 
+               (cdr (assoc 'value (json-read-from-string message_data)))))))
+
+(defun ssb-message-text (message_data)
+  (cdr (assoc 'text (cdr (assoc 'content
+                          (cdr (assoc 'value 
+                                      (json-read-from-string 
+                                       message_data))))))))
 
 (defun ssb-decode (message_id) 
   (shell-command-to-string (concat "sbot private.unbox " message_id)))
 
 
+(defun ssb-display (message_data)
+  (with-output-to-temp-buffer "Message"
+    (print (concat "Type: " (ssb-message-type message_data)))
+    (print (concat  "Time: " (ssb-message-timestamp message_data)))
+    (print (ssb-message-text message_data))
+    (print message_data)))
+
+
+(ssb-display (ssb-read-last id))
+
 (defun ssb-publish (text) 
   ; publish a message
   (shell-command-to-string 
    (concat "sbot publish --type post --text \"" text "\"")))
-
-(defun ssb-display (message_data)
-  (with-output-to-temp-buffer "Message"
-    (print (concat "Type: " (ssb-message-type message_data)))
-    (print message_data)))
-
-(ssb-display (ssb-read-last id))
 
 (defun ssb-quick-message (message)
   ; Create a quick message from the minibuffer.  No use of RET, /n only.
